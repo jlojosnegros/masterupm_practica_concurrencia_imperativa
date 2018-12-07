c91a17428b2e25b9aaac9183da4e52ebeb2dd9e3
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2018-12-07 11:09:18 +0100

    The test is wrong ... I think

--- src/test/java/es/sidelab/webchat/ChatManagerTest.java
+++ src/test/java/es/sidelab/webchat/ChatManagerTest.java
@@ -31,7 +31,7 @@ public class ChatManagerTest {
 		});
 
 		// Crear un nuevo chat en el chatManager
-		chatManager.newChat("Chat2", 5, TimeUnit.SECONDS);
+		chatManager.newChat("Chat", 5, TimeUnit.SECONDS);
 
 		// Comprobar que el chat recibido en el método 'newChat' se llama 'Chat'
 		Assertions.assertThat(chatName[0]).isEqualTo("Chat");

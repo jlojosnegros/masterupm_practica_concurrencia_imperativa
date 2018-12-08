a67b9a1d7be06376bafd786ac3c3ca92b3fd6fa2
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2018-12-08 12:42:43 +0100

    Some new functional test cases (without concurrency)

--- src/test/java/es/sidelab/webchat/ChatManagerTest.java
+++ src/test/java/es/sidelab/webchat/ChatManagerTest.java
@@ -1,5 +1,6 @@
 package es.sidelab.webchat;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Objects;
@@ -16,7 +17,7 @@ import es.codeurjc.webchat.User;
 public class ChatManagerTest {
 
 	@Test
-	public void newChat() throws InterruptedException, TimeoutException {
+	public void newChat() throws TimeoutException {
 
 		// Crear el chat Manager
 		ChatManager chatManager = new ChatManager(5);
@@ -34,7 +35,7 @@ public class ChatManagerTest {
 		chatManager.newChat("Chat", 5, TimeUnit.SECONDS);
 
 		// Comprobar que el chat recibido en el método 'newChat' se llama 'Chat'
-		Assertions.assertThat(chatName[0]).isEqualTo("Chat");
+		assertThat(chatName[0]).isEqualTo("Chat");
 	}
 
 	@Test
@@ -65,4 +66,74 @@ public class ChatManagerTest {
 				"user2".equals(newUser[0]));
 
 	}
+
+    @Test
+    public void newUser() {
+
+        ChatManager chatManager = new ChatManager(5);
+        final String[] newUser = new String[3];
+
+        TestUser user0 = new TestUser("user0") {
+            @Override
+            public void newUserInChat(Chat chat, User user) {
+                newUser[0] = user.getName();
+            }
+        };
+
+        TestUser user1 = new TestUser("user1") {
+            @Override
+            public void newUserInChat(Chat chat, User user) {
+                newUser[1] = user.getName();
+            }
+        };
+
+        TestUser user2 = new TestUser("user2") {
+            @Override
+            public void newUserInChat(Chat chat, User user) {
+                newUser[2] = user.getName();
+            }
+        };
+
+        chatManager.newUser(user0);
+        chatManager.newUser(user1);
+        chatManager.newUser(user2);
+
+        assertThat(chatManager.getUsers()).containsExactlyInAnyOrder(user0,user1,user2);
+        assertThat(newUser).containsExactly("user0","user1","user2");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_givenRepeatedUsers_ShouldThrowAnException() {
+        //given
+        ChatManager chatManager = new ChatManager(5);
+        final String[] newUser = new String[3];
+
+        TestUser user0 = new TestUser("user0") {
+            @Override
+            public void newUserInChat(Chat chat, User user) {
+                newUser[0] = user.getName();
+            }
+        };
+
+        TestUser user1 = new TestUser("user1") {
+            @Override
+            public void newUserInChat(Chat chat, User user) {
+                newUser[1] = user.getName();
+            }
+        };
+
+        TestUser user2 = new TestUser("user1") {
+            @Override
+            public void newUserInChat(Chat chat, User user) {
+                newUser[2] = user.getName();
+            }
+        };
+
+        //when
+        chatManager.newUser(user0);
+        chatManager.newUser(user1);
+        chatManager.newUser(user2);
+
+        //then should raise an exception
+    }
 }

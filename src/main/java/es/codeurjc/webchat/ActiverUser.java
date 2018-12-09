c4a9153f0b5d8bfad5cf3151fac0730e054bd87c
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2018-12-09 13:15:46 +0100

    New ActiveObject pattern for User.

---
+++ src/main/java/es/codeurjc/webchat/ActiverUser.java
@@ -0,0 +1,75 @@
+package es.codeurjc.webchat;
+
+import java.util.concurrent.*;
+
+public class ActiverUser implements User{
+
+    private final User wrappedUser;
+    private ExecutorService executorService;
+
+
+    public ActiverUser(User user) {
+        this.wrappedUser = user;
+        this.executorService = Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    public String getName() {
+        Future<String> ret = executorService.submit(wrappedUser::getName);
+        try {
+            return ret.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    @Override
+    public String getColor() {
+        Future<String> ret = executorService.submit(wrappedUser::getColor);
+        try {
+            return ret.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    @Override
+    public void newChat(Chat chat) {
+        executorService.execute(() -> wrappedUser.newChat(chat));
+    }
+
+    @Override
+    public void chatClosed(Chat chat) {
+        executorService.execute(() -> wrappedUser.chatClosed(chat) );
+    }
+
+    @Override
+    public void newUserInChat(Chat chat, User user) {
+        executorService.execute( () -> wrappedUser.newUserInChat(chat, user) );
+    }
+
+    @Override
+    public void userExitedFromChat(Chat chat, User user) {
+        executorService.execute( () -> wrappedUser.userExitedFromChat(chat, user) );
+    }
+
+    @Override
+    public void newMessage(Chat chat, User user, String message) {
+        executorService.execute(() -> wrappedUser.newMessage(chat, user, message) );
+    }
+
+    /// How to shutdown gracefully if java has no "destructor" ?
+    public void destructor() {
+
+        executorService.shutdown();
+        try {
+            boolean b = executorService.awaitTermination(1, TimeUnit.SECONDS);
+            ///@todo what to do with the boolean
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            executorService.shutdownNow();
+        }
+    }
+}

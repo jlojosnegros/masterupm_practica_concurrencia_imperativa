eb8f9c808f3a406b89888b8ec220b3926dccce54
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2018-12-13 17:06:59 +0100

    Add comments to the test cases

--- src/test/java/es/sidelab/webchat/ChatManagerTest.java
+++ src/test/java/es/sidelab/webchat/ChatManagerTest.java
@@ -53,7 +53,7 @@ public class ChatManagerTest {
 	@Test
 	public void test_newChat_Concurrent() {
 
-        // Crear el chat Manager
+        /// @Given: A chat manager with some users on it
 		ChatManager chatManager = new ChatManager(5);
 
 		ExecutorService executorService = Executors.newSingleThreadExecutor();
@@ -62,30 +62,29 @@ public class ChatManagerTest {
 
 		createCompletionUsersInChatManager(chatManager, NumberOfUsers, baseUserName, completionService);
 
-
-		// Crear un nuevo chat en el chatManager
+		/// @When: we create a new chat in the manager
 		createChatInChatManager("Chat", chatManager);
 
+		/// @Then ...
 		List<String> notifiedUsers = new ArrayList<>(chatManager.getUsers().size());
 		try {
-
+			///@Then message should be delivered to all users in chat manager ...
             for (int idx = 0; idx < NumberOfUsers; idx++) {
                 Future<CompletionUser.CompletionResult> take = completionService.poll(1,TimeUnit.SECONDS);
 
                 CompletionUser.CompletionResult completionResult = take.get(500, TimeUnit.MILLISECONDS);
 
-                assertThat(completionResult.getCalledUserName()).isIn(chatManager.getUsers()
-                        .stream()
-                        .map(User::getName)
-                        .collect(Collectors.toList())
-                );
+                ///@Then all users get called its newChat callback
+				assertThat(completionResult.getMethodCalled()).startsWith("newChat");
+				/// ... with the chat name as parameter
                 assertThat(completionResult.getIn_param()).containsExactly("Chat");
-                assertThat(completionResult.getMethodCalled()).startsWith("newChat");
+                /// ... and will not return anything meaningful
                 assertThat(completionResult.getReturnValue()).isNull();
 
                 notifiedUsers.add(completionResult.getCalledUserName());
             }
 
+            /// @Then All the users should be notified once and only once.
             assertThat(notifiedUsers).containsExactlyInAnyOrder(
 					chatManager.getUsers()
 							.stream()
@@ -129,7 +128,8 @@ public class ChatManagerTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void test_givenRepeatedUsers_ShouldThrowAnException() {
-        //given
+
+    	///@Given a chatManager and some users
         ChatManager chatManager = new ChatManager(5);
         final String[] newUser = new String[3];
 
@@ -154,20 +154,20 @@ public class ChatManagerTest {
             }
         };
 
-        //when
+        /// @When we add new users to the chatManager
         chatManager.newUser(user0);
         chatManager.newUser(user1);
         chatManager.newUser(user2);
 
-        //then should raise an exception
+        ///@Then should raise an exception
     }
 
 	@Test
 	public void test_removeChat_Concurrent() {
 
-		// Crear el chat Manager
+    	///@Given a chatManager ...
 		ChatManager chatManager = new ChatManager(5);
-
+		/// ,,, with some users that will notify when they execute any of its methods ...
 		ExecutorService executorService = Executors.newSingleThreadExecutor();
 		CompletionService<CompletionUser.CompletionResult> completionService =
 				new ExecutorCompletionService<>(executorService);
@@ -175,28 +175,34 @@ public class ChatManagerTest {
 		createCompletionUsersInChatManager(chatManager, NumberOfUsers, baseUserName, completionService);
 
 		final String ChatName = "Chat";
-		// Crear un nuevo chat en el chatManager
+
+		/// ... and a chat
 		createChatInChatManager(ChatName, chatManager);
 		consumeCompletionMessages(completionService);
 
-
-		// Delete created Chat
+		/// @When the chat is closed
 		chatManager.closeChat(chatManager.getChat(ChatName));
 
+
 		List<String> notifiedUserNames = new ArrayList<>(chatManager.getUsers().size());
+		/// @Then all the users should be notified
 		try {
 			for (int idx = 0; idx < NumberOfUsers; idx++) {
 				Future<CompletionUser.CompletionResult> take = completionService.poll(1,TimeUnit.SECONDS);
 
 				CompletionUser.CompletionResult completionResult = take.get(500, TimeUnit.MILLISECONDS);
 
-				assertThat(completionResult.getIn_param()).containsExactly(ChatName);
+				///@Then all users get called its chatClosed callback
 				assertThat(completionResult.getMethodCalled()).startsWith("chatClosed");
+				/// ... with the chat name as parameter
+				assertThat(completionResult.getIn_param()).containsExactly(ChatName);
+				/// ... and will not return anything meaningful
 				assertThat(completionResult.getReturnValue()).isNull();
 
 				notifiedUserNames.add(completionResult.getCalledUserName());
 			}
 
+			/// @Then All the users should be notified once and only once.
 			assertThat(notifiedUserNames).containsExactlyInAnyOrder(
 					chatManager.getUsers().stream()
 					.map(User::getName)
@@ -212,25 +218,25 @@ public class ChatManagerTest {
 	@Test
 	public void test_newUserInChat_Concurrent() {
 
-		// Crear el chat Manager
+		///@Given a chatManager ...
 		ChatManager chatManager = new ChatManager(5);
 
+		/// ... and some users that will notify when their methods are executed ...
 		ExecutorService executorService = Executors.newSingleThreadExecutor();
 		CompletionService<CompletionUser.CompletionResult> completionService =
 				new ExecutorCompletionService<>(executorService);
-
 		createCompletionUsersInChatManager(chatManager, NumberOfUsers, baseUserName, completionService);
 
+
 		final String ChatName = "Chat";
-		// Crear un nuevo chat en el chatManager
+		/// ... with a new chat where the created users are added
 		createChatInChatManager(ChatName, chatManager);
-
 		addExistingUsersToChat(chatManager, ChatName);
-
 
 		consumeCompletionMessages(completionService);
 
 
+		///@When we add a new user to the chat
 		final String newUserName = baseUserName + (NumberOfUsers+1);
 		addUserToChat(chatManager,
 				ChatName,
@@ -238,6 +244,7 @@ public class ChatManagerTest {
 		);
 
 
+		///@Then all the users should be notified ...
 		List<String> notifiedUserNames = new ArrayList<>(chatManager.getUsers().size());
 		try {
 			for (int idx = 0; idx < NumberOfUsers; idx++) {
@@ -246,13 +253,17 @@ public class ChatManagerTest {
 
 				CompletionUser.CompletionResult completionResult = take.get(500, TimeUnit.MILLISECONDS);
 
-				assertThat(completionResult.getIn_param()).containsExactly(ChatName,newUserName);
+				///@Then all users get called its newUserInChat callback
 				assertThat(completionResult.getMethodCalled()).startsWith("newUserInChat");
+				/// ... with the chat name as parameter
+				assertThat(completionResult.getIn_param()).containsExactly(ChatName,newUserName);
+				/// ... and will not return anything meaningful
 				assertThat(completionResult.getReturnValue()).isNull();
 
 				notifiedUserNames.add(completionResult.getCalledUserName());
 			}
 
+		    /// @Then All the users should be notified once and only once.
 			assertThat(notifiedUserNames).containsExactlyInAnyOrder(
 					chatManager.getUsers().stream()
 							.map(User::getName)
@@ -270,31 +281,32 @@ public class ChatManagerTest {
 	@Test
 	public void test_removeUserFromChat_Concurrent() {
 
-		// Crear el chat Manager
+    	///@Given a chatManager ...
 		ChatManager chatManager = new ChatManager(5);
 
 		ExecutorService executorService = Executors.newSingleThreadExecutor();
 		CompletionService<CompletionUser.CompletionResult> completionService =
 				new ExecutorCompletionService<>(executorService);
-
+		/// ... with some users that will notify when their functions are executed ...
 		createCompletionUsersInChatManager(chatManager, NumberOfUsers, baseUserName, completionService);
 
 		final String ChatName = "Chat";
-		// Crear un nuevo chat en el chatManager
+		/// ... with a chat already created ...
 		createChatInChatManager(ChatName, chatManager);
+		/// ... and all the already created added to the chat ...
 		addExistingUsersToChat(chatManager, ChatName);
 
-		//create a new user
+		///@When we create a new user and add it to the chat ...
 		final String newUserName = baseUserName + (NumberOfUsers+1);
 		User newUser = createCompletionUser(newUserName, completionService);
 		addUserToChat(chatManager, ChatName, newUser);
 
-
 		consumeCompletionMessages(completionService);
 
-		//remove user from chat
+		/// ... and we remove the newly user from the chat
 		chatManager.getChat(ChatName).removeUser(newUser);
 
+		///@Then all the remain users in chat should be notified.
 		List<String> notifiedUserNames = new ArrayList<>(chatManager.getUsers().size());
 		try {
 			for (int idx = 0; idx < NumberOfUsers; idx++) {
@@ -303,14 +315,15 @@ public class ChatManagerTest {
 
 				CompletionUser.CompletionResult completionResult = take.get(500, TimeUnit.MILLISECONDS);
 
-
-				assertThat(completionResult.getIn_param()).containsExactly(ChatName,newUserName);
+				///@Then ... the users get their callback called ...
 				assertThat(completionResult.getMethodCalled()).startsWith("userExitedFromChat");
+				/// ... with the name of the removed user and the name of the chat it has been removed from ...
+				assertThat(completionResult.getIn_param()).containsExactly(ChatName,newUserName);
 				assertThat(completionResult.getReturnValue()).isNull();
 
 				notifiedUserNames.add(completionResult.getCalledUserName());
 			}
-
+			///@Then ... all the remaining users in chat should be notified once and only once
 			assertThat(notifiedUserNames).containsExactlyInAnyOrder(
 					chatManager.getUsers().stream()
 							.map(User::getName)
@@ -326,36 +339,35 @@ public class ChatManagerTest {
 	@Test
 	public void test_newMessageInChat_Concurrent() {
 
-		// Crear el chat Manager
+		///@Given a chat manager ...
 		ChatManager chatManager = new ChatManager(5);
 
 		ExecutorService executorService = Executors.newSingleThreadExecutor();
 		CompletionService<CompletionUser.CompletionResult> completionService =
 				new ExecutorCompletionService<>(executorService);
-
+		/// ... and some users that will report when their methods are executed ...
 		createCompletionUsersInChatManager(chatManager, NumberOfUsers, baseUserName, completionService);
 
+		/// ... and a new chat ...
 		final String ChatName = "Chat";
-		// Crear un nuevo chat en el chatManager
 		Chat chat = createChatInChatManager(ChatName, chatManager);
-
-
+		/// ... with all the currently existing users added in the chat..
 		addExistingUsersToChat(chatManager, ChatName);
 
 
-		//create a new user
+		/// @When we create a new user ...
 		final String newUserName = baseUserName + (NumberOfUsers+1);
 		User newUser = createCompletionUser(newUserName, completionService);
+		/// ... add it to the chat ...
 		addUserToChat(chatManager, ChatName, newUser);
-
-
 
 		consumeCompletionMessages(completionService);
 
+		/// ... and send a message to the chat from the most recently created user
 		final String messagePayload = "new message to chat";
 		chat.sendMessage(newUser,messagePayload);
 
-
+		///@Then all the users in the chat should be notified
 		List<String> notifiedUsers = new ArrayList<>(NumberOfUsers);
 		try {
 			for (int idx = 0; idx < NumberOfUsers; idx++) {
@@ -364,20 +376,16 @@ public class ChatManagerTest {
 
 				CompletionUser.CompletionResult completionResult = take.get(500, TimeUnit.MILLISECONDS);
 
-				assertThat(completionResult.getCalledUserName()).isIn(chatManager.getUsers()
-						.stream()
-						.map(User::getName)
-						.collect(Collectors.toList())
-				);
-				assertThat(completionResult.getIn_param()).containsExactly(ChatName,
-						newUserName,
-						messagePayload);
+				///@Then ... their callback gets called ...
 				assertThat(completionResult.getMethodCalled()).startsWith("newMessage");
+				/// ... with the name of the user who sent the message, the message itself and the name of the chat
+				assertThat(completionResult.getIn_param()).containsExactly(ChatName, newUserName, messagePayload);
 				assertThat(completionResult.getReturnValue()).isNull();
 
 				notifiedUsers.add(completionResult.getCalledUserName());
 			}
 
+			///@Then all the users should be notified once and only once
 			assertThat(notifiedUsers).containsExactlyInAnyOrder(chatManager.getUsers()
 					.stream()
 					.map(User::getName)
@@ -390,13 +398,23 @@ public class ChatManagerTest {
 
 	}
 
+	/**
+	 * Add a User to a Chat in a ChatManager.
+	 * @param chatManager
+	 * @param chatName
+	 * @param newUser
+	 */
 	private void addUserToChat(ChatManager chatManager,
 							   final String chatName,
 							   final User newUser) {
-		//chatManager.newUser(newUser);
 		chatManager.getChat(chatName).addUser(newUser);
 	}
 
+	/**
+	 * Go throw all the users in a chatManager and add them to the Chat with name equals to chatName.
+	 * @param chatManager
+	 * @param chatName
+	 */
 	private void addExistingUsersToChat(ChatManager chatManager, final String chatName) {
 		chatManager.getUsers().forEach(user -> chatManager.getChat(chatName).addUser(user));
 
@@ -406,6 +424,10 @@ public class ChatManagerTest {
 		);
 	}
 
+	/**
+	 * Receive all the remaining messages in the completionService to avoid errors and noise in the channel.
+	 * @param completionService
+	 */
 	private void consumeCompletionMessages(CompletionService<CompletionUser.CompletionResult> completionService) {
 		try {
 
@@ -415,17 +437,17 @@ public class ChatManagerTest {
 				 take = completionService.poll(1,TimeUnit.SECONDS)){
 				LoggerFactory.getLogger(getClass()).info("consumed:" + take.get().toString());
 			}
-//			for (int idx = 0; idx < NumberOfUsers; idx++) {
-//				Future<CompletionUser.CompletionResult> take = completionService.poll(1,TimeUnit.SECONDS);
-////				assertThat(take).isNotNull();
-////				CompletionUser.CompletionResult completionResult = take.get(500, TimeUnit.MILLISECONDS);
-//			}
-
 		} catch (InterruptedException | ExecutionException e) {
 			fail(e.getMessage());
 		}
 	}
 
+	/**
+	 * Creates a new Chat with name chatName in the chatManager.
+	 * @param chatName
+	 * @param chatManager
+	 * @return the newly created Chat
+	 */
 	private Chat createChatInChatManager(String chatName, ChatManager chatManager) {
 		try {
 			return chatManager.newChat(chatName, 5, TimeUnit.SECONDS);
@@ -435,16 +457,31 @@ public class ChatManagerTest {
 		return null;
 	}
 
+	/**
+	 * Creates users of CompletionUser class and add them to the chatManager.
+	 *
+	 * The name of each user will be the baseUserName and a number in [0,numberOfUsers)
+	 *
+	 * @param chatManager
+	 * @param numberOfUsers Number of users to be added to chatManager
+	 * @param baseUserName  String to be used to build the name of the created users
+	 * @param completionService
+	 */
 	private void createCompletionUsersInChatManager(ChatManager chatManager,
-													int numberOfUsers,
-													String baseUserName,
+													final int numberOfUsers,
+													final String baseUserName,
 													CompletionService<CompletionUser.CompletionResult> completionService) {
 		for (int idx = 0; idx < numberOfUsers; idx++) {
-
 			chatManager.newUser(createCompletionUser(baseUserName+idx, completionService));
 		}
 	}
 
+	/**
+	 * Just create a new CompletionUser using UserBuilder.
+	 * @param newUserName the name of the created user.
+	 * @param completionService
+	 * @return The created User.
+	 */
 	private User createCompletionUser(String newUserName, CompletionService<CompletionUser.CompletionResult> completionService) {
 		User user = null;
 		try {
